421750505aeb7abec3429570ef0ec2329372fdeb
Mikhail <merzlickinm@yandex.ru>
2024-12-10 00:39:52 +0300

    разность и сумма

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@ int main() {
     std::cin >> num1 >> num2;
 
     int razn = num1 - num2;
-
+    int sum = num1 + num2;
     std::cout << "1 chislo: " << num1 << std::endl;
     std::cout << "2 chislo: " << num2 << std::endl;
     std::cout << "Razn: " << razn << std::endl;
-
+    std::cout << "Sum: " << sum << std::endl;
     return 0;
 }
